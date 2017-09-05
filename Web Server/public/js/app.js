f032281f76564a6c324292eb57ddbeb4d6400813
lidorbt <lidorb98@gmail.com>
2017-09-05 19:00:00 +0300

    jut

--- Web Server/public/js/app.js
+++ Web Server/public/js/app.js
@@ -5,5 +5,20 @@ app.config(function ($routeProvider) {
         .when('/', {
             controller: 'indexCtrl',
             templateUrl: '/views/main.html'
+        })
+        .when('/control', {
+            controller: 'indexCtrl',
+            templateUrl: '/views/main.html'
+        })
+        .when('/team', {
+            controller: 'indexCtrl',
+            templateUrl: '/views/team.html'
+        })
+        .when('/setup', {
+            controller: 'indexCtrl',
+            templateUrl: '/views/setup.html'
+        })
+        .otherwise({
+            redirect: '/'
         });
 })
