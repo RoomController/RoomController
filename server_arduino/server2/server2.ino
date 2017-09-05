33fdca615206d2296e1eff3a5f802ffc4246c373
asafyos <asafsaf3@gmail.com>
2017-09-05 18:57:59 +0300

    a

--- server_arduino/server2/server2.ino
+++ server_arduino/server2/server2.ino
@@ -3,6 +3,7 @@ IRsend irsend;
 
 const int NUM_OF_NUMS = 9;
 const char START_OF_TEXT_PRINT = '-';
+
 long numbers[NUM_OF_NUMS] = {0xFF30CF, 0xFF18E7, 0xFF7A85, 0xFF10EF, 0xFF38C7, 0xFF5AA5, 0xFF42BD, 0xFF4AB5, 0xFF52AD};
 
 String getSerialRead()
@@ -39,16 +40,21 @@ int getNumFromText(String text) {
 }
 
 void printText(String text) {
-  Serial.print("printing: ");
-  Serial.println(text);
+  Serial.println("printing: ");
   irsend.sendSony(0xFF01FF, 24);
-  delay(200);
-  for (int i = 1; i < text.length() && i < 32; i++) {
-    irsend.sendSony(text[i], 8);
-    delay(200);
+  delay(2000);
+  for (int i = 1; i < text.length() && i <= 32; i++) {
+    Serial.print("FFFF");
+    Serial.println(text[i], HEX);
+    int sentVal = 0xFFFF;
+    sentVal *= 0x100;
+    sentVal += text[i];
+    irsend.sendSony(sentVal, 24);
+    delay(2000);
   }
+  Serial.println("------");
   irsend.sendSony(0xFF01FF, 24);
-  delay(200);
+  delay(2000);
 }
 
 void setup()
@@ -69,12 +75,12 @@ void loop()
     } else {
       if(command.equals("off") || command == "o" || command == "O") {
           irsend.sendSony(0xFF906F, 24);
-      } else if (command[0] == START_OF_TEXT_PRINT) {
-        printText(command);
-      } else {
-        String txt = (String)START_OF_TEXT_PRINT;
-        txt += "Command not found";
-        printText(txt);
+//      } else if (command[0] == START_OF_TEXT_PRINT) {
+//        printText(command);
+//      } else {
+//        String txt = (String)START_OF_TEXT_PRINT;
+//        txt += "Command not found";
+//        printText(txt);
       }
     }
     delay(100);
