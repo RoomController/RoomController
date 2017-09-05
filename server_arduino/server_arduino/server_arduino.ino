d9f22e6b86543ccc557d9d5fccc082a02a9ee65d
asafyos <asafsaf3@gmail.com>
2017-09-05 13:03:29 +0300

    arduino transmitter

---
+++ server_arduino/server_arduino/server_arduino.ino
@@ -0,0 +1,19 @@
+#include <IRremote.h>
+IRsend irsend;
+
+const int NUM_OF_LEDS = 9;
+long resaults[NUM_OF_LEDS] = {0xFF30CF, 0xFF18E7, 0xFF7A85, 0xFF10EF, 0xFF38C7, 0xFF5AA5, 0xFF42BD, 0xFF4AB5, 0xFF52AD};
+char serialRead;
+
+void setup()
+{
+ Serial.begin(9600);
+}
+
+void loop() 
+{
+    serialRead = Serial.read();
+    Serial.println(serialRead);
+    int index = (int)serialRead;
+    irsend.sendSony(resaults[index], 24);
+}
